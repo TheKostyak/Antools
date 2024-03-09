a754ec2c38988d88729597dc01e928e0c8841720
Kostya <87530514+TheKostyak@users.noreply.github.com>
2024-03-09 23:17:12 +0100

    add navigation for mobile devices

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,16 @@
 const burger = document.querySelector(".burger");
-burger.addEventListener("click",()=>{
+const nav = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
+burger.addEventListener("click", (e) => {
     burger.classList.toggle("burger_active");
+    nav.classList.toggle("nav_active");
+    overlay.classList.toggle("overlay_active");
+});
+overlay.addEventListener("click", ()=>{
+    burger.classList.toggle("burger_active");
+    nav.classList.toggle("nav_active");
+    overlay.classList.toggle("overlay_active");
 })
+
+
+
